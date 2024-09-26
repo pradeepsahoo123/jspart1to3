08c6b88a59d68afaac9fb345bd1b2af6fae7cf87
pradeepsahoo123 <146757131+pradeepsahoo123@users.noreply.github.com>
2024-09-26 11:42:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,883 @@
+// console.log("sahoo");
+// console.log("arobindo");
+// let a=5;
+// let b=12;
+// console.log("sum as=", b-a);
+
+// let pencilPrice=5;
+// let erasorPrice=10;
+
+// console.log(`The total price is: ${pencilPrice + erasorPrice} Rupees`); 
+
+//arithmetic opetion
+
+// let age=30;
+// console.log(age>24);
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// console.log(a>b);
+
+// console.log("before a if statement");
+
+// let firstname="sahoo";
+// if(firstname ="sahoo") {
+//     console.log(`welcome ${firstname}`)
+// }
+
+// let age = 16;
+
+// if(age>=20) {
+//     console.log("can drive");
+// }
+
+// if(age<18){
+//     console.log("can't drive");
+// }
+// console.log("after a if statement");
+
+//..practic qus  traficlight..
+// let color ="yellow";
+
+// if(color=="red") {
+//     console.log("stop");
+// }
+
+// if(color=="yellow"){
+//     console.log("start for kye");
+// }
+// if(color=="green"){
+// console.log("goo....");
+// }
+
+// let mark=92;
+
+// if(mark>=80){
+//     console.log("grad:A");
+// }
+// else if(mark>=60){
+//     console.log("grad:B");
+// }
+// else if(mark>=33){
+//     console.log("just pass");
+// }
+// else{
+//     console.log("butterluck next time");
+// }
+
+
+
+// let mark=45;
+
+// if (mark>=80){
+//     console.log("pass"); {
+// }
+//          if(mark>=60){
+//             console.log("grad:B");
+//                     }
+//                 else {
+//                     console.log("just pass");
+//                     }
+//                 }
+//     else {
+//     console.log("butterluck next time");
+// }
+
+// let mark=75;
+
+// if(mark>=35&&mark>=50){
+//     console.log("pass");
+// }
+
+// let mark=5;
+
+// if(mark>=35 || mark>=80){
+//     console.log("pass");
+// }
+
+// let mark=75;
+
+//  if((mark>=35 && mark<=80) || !false){
+
+
+//      console.log("pass");
+//     }
+
+// !(true);
+
+//practis qs..
+
+// let num=12;
+
+// if((num%3===0)  && ( (num+1===15) || (num-1===11) ) ) {
+//     console.log("safe");
+// }
+
+// else{
+//     console.log("unsave");
+// }
+
+
+// alert("scam innnnnn");
+
+// console.error("its err");
+
+
+// console.warn("its warning");
+
+// prompt("enter rool no.");
+
+// let firstname = prompt("enter your name : ");
+// let lastname = prompt("enter your name : ");
+
+// console.log( "welcome"  + firstname + "  " + lastname + "!");
+
+//assignment qs 1...
+
+// let num=20;
+// if(num % 10=== 0){
+//     console.log("good");
+// }
+// else{
+//     console.log("bad");
+// }
+
+// let name=prompt("plz enter your name");
+// let age=prompt("plz enter yuor age");
+// alert(`$(name) is $(age) ypur old.`);
+
+// let name="golden";
+// if((str=='g') && (str.length>5) ){
+//     console.log("golden");
+// }
+// else{
+//     console.log("not golden");
+// }
+
+// let a=3;
+// let b=31;
+// let c=13;
+
+// if (a>b){
+//     if(a>c){
+//         console.log(a, "is largest");
+//     }
+//     else{
+//         console.log(c, "is largest");
+//     }
+// }
+// else{
+//     if(b>c){
+//         console.log(b,"is largest");
+//     }
+//     else{
+//         console.log(c,"is largest");
+//     }
+
+
+
+//////js part3.......................
+
+
+// let msg="   hello   ";
+// let newMsg= msg.trim();
+// console.log(newMsg);
+
+//  let str="iloveyou";
+//    console.log(str.slice(0,11));
+//  console.log(str.slice-1);
+
+// str
+// "iloveyou"
+// str.slice(-4)
+// "eyou"
+// str.replace("o", "k" ,"o","k")
+// "ilkveyou"
+// str.repeat(10)
+// "iloveyouiloveyouiloveyouiloveyouiloveyouiloveyouiloveyouiloveyouiloveyouiloveyou"
+// str.replace("o", "k").replace("o", "k")
+// "ilkveyku" 
+
+// let nums=[14,8,6,34];
+// name
+//  Array(3) [ "ps", "sa", "sk" ]
+
+// name[0]
+// "ps"
+// name[2]
+// "sk"
+// name.length
+// 3 
+
+
+// let arr=[];
+
+// let primary=["red","yellow","blue"];
+
+// let nums=[[2,4],[3,6],[4,8]];
+
+// let game=[['x',null,'0'],['x',null,'0']['x',null,'0']];
+
+// let str = prompt("please enter a string");
+// if(str.length == 0) {
+// console.log("string is empty");
+// } else {
+
+// console.log("string is not empty");
+// }
+
+// let str = "ApNaCoLlEgE";
+// let idx = 3;
+// if(str[idx] == str[idx].toUpperCase()) {
+// console.log("character is lowercase");
+// } else {
+// console.log("character is not lowercase");
+// }
+
+// let arr = ["hello", 'a', 23, 64, 99, -6];
+// let item = 64;
+// if(arr.indexOf(item) != -1) {
+
+// console.log("element exists in array");
+// } else {
+// console.log("element doesn't exist in array");
+// }
+
+
+
+
+
+/////////js part3.................
+
+
+// for(let iloveu=10; iloveu>=1;iloveu=iloveu-2){
+//     console.log(iloveu);
+// }
+
+
+// for(let iloveu=1; iloveu<=15;iloveu=iloveu+2) {
+//     console.log(iloveu);////odd no.
+// }
+
+
+//  for(let iloveu=200; iloveu>=1;iloveu=iloveu-2) {
+//      console.log(iloveu);////even no.
+//  }
+
+// for(let i=1;i>=0;i++) {
+//     console.log(i);////infinite loop.
+// }
+
+
+// let n=prompt("enter the no.");
+// n=parseInt(n);
+
+// for( let i=n; i<=n; i=i+n){
+//     console.log(i);
+
+// }
+
+// for(i=1; i<=3; i++) {
+//      console.log(`outter loop `);
+//     for(j=1; j<=3; j++){
+//         console.log(j);
+//     }
+// }
+
+let i=1;
+while (i<=5);
+console.log(i);
+i++;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
